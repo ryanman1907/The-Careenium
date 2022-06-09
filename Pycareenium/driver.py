a0c5052e6aaf0a967813683216b0da5f466af6c7
Ryan Henderson <ryan.henderson@duke.edu>
2022-06-09 01:00:41 -0600

    added to simm

--- Pycareenium/driver.py
+++ Pycareenium/driver.py
@@ -1,12 +1,18 @@
 import pygame as pg
 from statistical_mentalics import *
 
+background_color = "black"
+
 pg.init()
 screen = pg.display.set_mode((400, 800))
 pg.display.update()
 
 thinking = True
+
+test_simm = Simm(5, 100, 200, 0, 5)
+
 while thinking:
     for event in pg.event.get():
+
         if event.type == pg.QUIT:
             thinking = False
--- Pycareenium/simmballism.py
+++ Pycareenium/simmballism.py
@@ -1,16 +1,27 @@
-from driver import screen
+from driver import screen, background_color
 import pygame as pg
 
 class Simm:
-    def __init__(self, size, x, y, v_x, v_y, mass):
-        self.size = size
+
+    def __init__(self, radius, x, y, v_x, v_y, mass, color):
+        self.radius = radius
         self.x = x
         self.y = y
         self.v_x = v_x
         self.v_y = v_y
         self.mass = mass
+        self.color = color
     
-    def show(self):
+    def show(self, color):
         global screen
-        pg.draw.circle(screen, "pink", self.x, self.y)
+        pg.draw.circle(screen, color, (self.x, self.y), self.radius)
+
+    def think(self):
+        self.show(pg.color(background_color))
+
+        self.x += self.v_x
+        self.y += self.v_y
+
+        self.show(pg.color(self.color))
+
 
